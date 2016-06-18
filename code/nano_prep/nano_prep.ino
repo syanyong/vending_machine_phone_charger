697a7509407a66df21028eb44ce54a56d29963d4
Sarucha Yanyong <sarucha.yanyong@th.transport.bombardier.com>
2016-06-18 17:49:21 +0700

    nano_prep is tested, all error have been solved.; Add nano_MCP23017 as submodule;

--- code/nano_prep/nano_prep.ino
+++ code/nano_prep/nano_prep.ino
@@ -1,32 +1,94 @@
 /* INCLUDE */
 #include <SPI.h>
+#include "Adafruit_MCP23017.h"
 
 
 /* DEFINE */
 #define PIN_SS1       10
 #define PIN_SS2       9
 
+#define MCP_IN11      0
+#define MCP_IN12      1
+#define MCP_IN13      2
+
+#define MCP_IN21      3
+#define MCP_IN22      4
+#define MCP_IN23      5
+
+#define MCP_IN31      6
+#define MCP_IN32      7
+#define MCP_IN33      8
+
+#define MCP_IN41      9
+#define MCP_IN42      10
+#define MCP_IN43      11
+
+#define MCP_OUT01     12
+#define MCP_OUT02     13
+#define MCP_OUT03     14
+#define MCP_OUT04     15
+
+/* DECLARE */
+Adafruit_MCP23017 mcp;
 
 /* VARIABLE */
-int i = 0;
 
 
 void setup() {
     initMAX7219();
-}
+    setupIOEx();
+} 
 
 
 void loop() {
-    printSeg(0, i);
-    printSeg(1, 9999-i);
-    _delay_ms(100);
-    i++;
-    if (i > 9999){
-      i=0;
-    }
+
 }
 
 /* FUNCTION **************************************************/
+void setupIOEx(){
+  mcp.begin(); 
+  
+  /* Input */
+  mcp.pinMode(MCP_IN11, INPUT); 
+  mcp.pullUp(MCP_IN11, HIGH);
+  mcp.pinMode(MCP_IN12, INPUT); 
+  mcp.pullUp(MCP_IN12, HIGH);
+  mcp.pinMode(MCP_IN13, INPUT); 
+  mcp.pullUp(MCP_IN13, HIGH);
+
+  mcp.pinMode(MCP_IN21, INPUT); 
+  mcp.pullUp(MCP_IN21, HIGH);
+  mcp.pinMode(MCP_IN22, INPUT); 
+  mcp.pullUp(MCP_IN22, HIGH);
+  mcp.pinMode(MCP_IN23, INPUT); 
+  mcp.pullUp(MCP_IN23, HIGH);
+  
+  mcp.pinMode(MCP_IN31, INPUT); 
+  mcp.pullUp(MCP_IN31, HIGH);
+  mcp.pinMode(MCP_IN32, INPUT); 
+  mcp.pullUp(MCP_IN32, HIGH);
+  mcp.pinMode(MCP_IN33, INPUT); 
+  mcp.pullUp(MCP_IN33, HIGH);
+
+  mcp.pinMode(MCP_IN41, INPUT); 
+  mcp.pullUp(MCP_IN41, HIGH);
+  mcp.pinMode(MCP_IN42, INPUT); 
+  mcp.pullUp(MCP_IN42, HIGH);
+  mcp.pinMode(MCP_IN43, INPUT); 
+  mcp.pullUp(MCP_IN43, HIGH);
+  
+  /* Output */
+  mcp.pinMode(MCP_OUT01, OUTPUT);
+  mcp.pinMode(MCP_OUT02, OUTPUT);
+  mcp.pinMode(MCP_OUT03, OUTPUT);
+  mcp.pinMode(MCP_OUT04, OUTPUT);
+
+  mcp.digitalWrite(MCP_OUT01, LOW);
+  mcp.digitalWrite(MCP_OUT02, LOW);
+  mcp.digitalWrite(MCP_OUT03, LOW);
+  mcp.digitalWrite(MCP_OUT04, LOW);
+}
+
 /*
  * @brief: Initial IC MAX7219 (7 Segment driver)
  */
@@ -92,3 +154,30 @@ void printSeg(uint8_t seg_no, unsigned short num){
       num = num/10;
     }
 }
+
+//void test7seg(unsigned short *i){
+//    printSeg(0, *i);
+//    printSeg(1, 9999-*i);
+//    _delay_ms(100);
+//    *i++;
+//    if (*i > 9999){
+//      *i=0;
+//    }
+//}
+
+//void testIO(){
+////  mcp.digitalWrite(MCP_OUT01, HIGH);
+////  _delay_ms(200);
+////  mcp.digitalWrite(MCP_OUT01, LOW);
+////  _delay_ms(200);   
+////    tmp = mcp.digitalRead(MCP_IN23);
+////  tmp = mcp.readGPIOAB();
+////  Serial.println( mcp.digitalRead(MCP_IN23) );
+//  tmp = mcp.readGPIOAB() & 0x0FFF ;
+//  Serial.println(tmp);
+//  
+//}
+
+
+
+
