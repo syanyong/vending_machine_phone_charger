b3965b5e9864c1febcd2c068e28ea513158c153e
Sarucha Yanyong <sarucha.yanyong@th.transport.bombardier.com>
2016-06-20 00:35:06 +0700

    Add sudosx_MAX7219.h

--- code/nano_prep/nano_prep.ino
+++ code/nano_prep/nano_prep.ino
@@ -1,12 +1,9 @@
 /* INCLUDE */
-#include <SPI.h>
+#include "sudosx_MAX7219.h"
 #include "Adafruit_MCP23017.h"
 
 
 /* DEFINE */
-#define PIN_SS1       10
-#define PIN_SS2       9
-
 #define MCP_IN11      0
 #define MCP_IN12      1
 #define MCP_IN13      2
@@ -35,13 +32,18 @@ Adafruit_MCP23017 mcp;
 
 
 void setup() {
-    initMAX7219();
-    setupIOEx();
+    setupMAX7219();
+//    setupIOEx();
+
+    printSeg(1, 1);
+    printSeg(2, 2);
+    printSeg(3, 3);
+    printSeg(4, 4);
 } 
 
 
 void loop() {
-
+  _delay_ms(200);
 }
 
 /* FUNCTION **************************************************/
@@ -88,96 +90,4 @@ void setupIOEx(){
   mcp.digitalWrite(MCP_OUT03, LOW);
   mcp.digitalWrite(MCP_OUT04, LOW);
 }
-
-/*
- * @brief: Initial IC MAX7219 (7 Segment driver)
- */
-void initMAX7219(){
-  SPI.begin();
-  pinMode(PIN_SS1, OUTPUT);
-  pinMode(PIN_SS2, OUTPUT);
-  
-  /* Enable Testing Mode */
-  spiSend (PIN_SS1, 0xF, 0x1);
-  spiSend (PIN_SS2, 0xF, 0x1);
-  _delay_ms(250);
-  /* Disable Testing Mode */
-  spiSend (PIN_SS1, 0xF,0x0);
-  spiSend (PIN_SS2, 0xF,0x0);
-  /* Go normal operation */
-  spiSend (PIN_SS1, 0xC,0x1);
-  spiSend (PIN_SS2, 0xC,0x1);
-  /* Set Scan Limit */
-  spiSend (PIN_SS1, 0xB,0x7);
-  spiSend (PIN_SS2, 0xB,0x7);
-  /* Decode mode that you shall be send the number charecter. */
-  spiSend (PIN_SS1, 0x9,0xFF);
-  spiSend (PIN_SS2, 0x9,0xFF);
-  /* Set Intensity */
-  spiSend (PIN_SS1, 0xA, 0x6);
-  spiSend (PIN_SS2, 0xA, 0x6);
-
-}
-
-
-/*
- * @brief: Send data via SPI (Serial Peripheral Interface).
- */
-void spiSend(uint8_t pin_ss, uint8_t command, unsigned short value)
-{
-  digitalWrite(pin_ss, LOW); //chip select is active low
-  SPI.transfer(command);
-  SPI.transfer(value);
-  digitalWrite(pin_ss,HIGH);
-}
-
-
-/*
- * @brief: Print 7 Segment, support four 4DIGIT.
- */
-void printSeg(uint8_t seg_no, unsigned short num){
-    for (int8_t i=0; i <= 3; i++)
-    {
-        byte character = num % 10; //get the value of the rightmost digit
-        if (num == 0 && i > 0)
-        character = 0xf;
-        switch(seg_no){
-            case 0: spiSend(PIN_SS1, 4-i, character);
-            break;
-            case 1: spiSend(PIN_SS1, 8-i, character);
-            break;
-            case 2: spiSend(PIN_SS2, 4-i, character);
-            break;
-            case 3: spiSend(PIN_SS2, 8-i, character);
-            break;
-        }
-      num = num/10;
-    }
-}
-
-//void test7seg(unsigned short *i){
-//    printSeg(0, *i);
-//    printSeg(1, 9999-*i);
-//    _delay_ms(100);
-//    *i++;
-//    if (*i > 9999){
-//      *i=0;
-//    }
-//}
-
-//void testIO(){
-////  mcp.digitalWrite(MCP_OUT01, HIGH);
-////  _delay_ms(200);
-////  mcp.digitalWrite(MCP_OUT01, LOW);
-////  _delay_ms(200);   
-////    tmp = mcp.digitalRead(MCP_IN23);
-////  tmp = mcp.readGPIOAB();
-////  Serial.println( mcp.digitalRead(MCP_IN23) );
-//  tmp = mcp.readGPIOAB() & 0x0FFF ;
-//  Serial.println(tmp);
-//  
-//}
-
-
-
 
